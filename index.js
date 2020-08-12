a875ace2b8da65d633d953a385fd32802cad9bee
boom100100 <bsd231@nyu.edu>
2020-08-12 18:40:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function largestSubarraySum(array){
-  
+
   let target = 0;
   let reversedArray = [...array].reverse();
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-	for (let i = 0; i < (array.length - 1); i++){
-      const arr1Sum = array.slice(i).reduce(reducer);
-      const arr2Sum = reversedArray.slice(i).reduce(reducer);
+	for (let i = 0; i < array.length - 1; i++){
+      const slicedArray = array.slice(i);
+      const slicedReversedArray = reversedArray.slice(i);
 
-      const arr3Sum = array.slice(i, -1).reduce(reducer);
-      const arr4Sum = reversedArray.slice(i, -1).reduce(reducer);
+      const arr1Sum = slicedArray.reduce(reducer);
+      const arr2Sum = slicedReversedArray.reduce(reducer);
+
+      const arr3Sum = slicedArray.slice(0, -1).reduce(reducer);
+      const arr4Sum = slicedReversedArray.slice(0, -1).reduce(reducer);
 
       target < arr1Sum ? target = arr1Sum : undefined;
       target < arr2Sum ? target = arr2Sum : undefined;
